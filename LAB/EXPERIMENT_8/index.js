bd9b668d93c0cc0d1ac2e0320a92f645e96bfd0c
shahidkx07 <shahidkhan88892@gmail.com>
2025-11-11 13:43:59 +0530

    password encryption completed with postman for sending data

--- LAB/EXPERIMENT_8/index.js
+++ LAB/EXPERIMENT_8/index.js
@@ -2,6 +2,7 @@ const express =require('express');
 const app=express();
 const mongoose=require('mongoose');
 const port =3000;
+const bcrypt = require('bcrypt');
 const fs=require('fs');
 const path=require('path');
 
@@ -29,6 +30,32 @@ mongoose.connect(atlasuri).then(()=>{
 const userschema=new mongoose.Schema({
     username:{type:'String',required:true},
     password:{type:'String',required:true},
+})
+
+//pre is a hook that allows us to modify the document before saving it to d.b.
+// 'save' refers to Mongoose save event
+//next is a callback function called when middleware work is done as flow goes to next middleware or route
+//whenever you use next mongoose automatically creates a callback function you just have to name it, here i named it as next
+userschema.pre('save',async function (next) {
+
+    // block is mainly used when updating
+    if (!this.isModified('password')){
+        return next()
+    }
+     
+
+    //this is the main part for hashing password
+    try {
+        const salt =await bcrypt.genSalt(10);
+        this.password= await bcrypt.hash(this.password,salt);
+        next()
+    }
+    //catch block executes if any line in try block fails
+    // next(err) this aborts the database operation
+    catch(err){
+        next(err);
+    }
+    
 })
 //creating a model based on schema
 const model=mongoose.model('exp8',userschema);
