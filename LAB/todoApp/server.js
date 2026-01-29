853da429da115a3c6cb5c82d5e2e97f5c5b96677
Shahid Khan <161334439+shahidk07@users.noreply.github.com>
2026-01-29 12:39:31 +0530

    Change server port from 3000 to 8080

--- LAB/todoApp/server.js
+++ LAB/todoApp/server.js
@@ -5,7 +5,7 @@ const express =require('express');
 const app=express();
 const mongoose=require('mongoose');
 const session = require('express-session');
-const PORT =process.env.PORT||3000;
+const PORT =process.env.PORT||8080;
 const bcrypt = require('bcrypt');
 const path=require('path');
 
